b9db84ffdcd059f7b9b1a83580560fa3c8c146da
KrakenM (T) <moemyatag05@gmail.com>
2025-05-21 14:50:37 -0700

    Add connection to 'MainDatabase' through mongoose'

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -1,4 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "MainDatabase") // connect to Db "users"
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;

71cfc0430749c622baba9e392743bbd7a0e9cbbe
David Cruz <david@codebycruz.com>
2025-05-22 23:20:16 -0700

    Finish up jwts and authentication

--- packages/backend/src/backend.ts
+++ packages/backend/src/backend.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import { findAllUsers } from "./services/user";
+import { createUser, findAllUsers, findUserByEmail, findUserByName, generateJWT, isPasswordValid, verifyJWT } from "./services/user";
 
 mongoose.set("debug", true);
 mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}/MainDatabase`).catch((err) => {
@@ -20,17 +20,91 @@ app.get("/", (req, res) => {
 });
 
 app.post("/auth/register", async (req, res) => {
-    const { email, password } = req.body;
+    const username = req.body?.username;
+    const email = req.body?.email;
+    const password = req.body?.password;
+
+    const errors: string[] = [];
+    if (typeof email !== "string") {
+        errors.push("Email must be a string");
+    }
+    if (typeof password !== "string") {
+        errors.push("Password must be a string");
+    }
+    if (typeof username !== "string") {
+        errors.push("Username must be a string");
+    }
+
+    if (errors.length > 0) {
+        res.status(400).send(errors.join(", "));
+        return;
+    }
+
+    if (await findUserByName(username) || await findUserByEmail(email)) {
+        res.status(400).send("User already exists");
+        return;
+    }
+
+    const user = await createUser(email, password, username);
+    const token = generateJWT(user, "30m");
+
+    res.status(201).json({ token, user });
 });
 
 app.post("/auth/login", async (req, res) => {
-    const { email, password } = req.body;
+    const email = req.body?.email;
+    const password = req.body?.password;
+
+    const errors: string[] = [];
+    if (typeof email !== "string") {
+        errors.push("Email must be a string");
+    }
+    if (typeof password !== "string") {
+        errors.push("Password must be a string");
+    }
+
+    if (errors.length > 0) {
+        res.status(400).send(errors.join(", "));
+        return;
+    }
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+        res.status(400).send("User not found");
+        return;
+    }
+
+    if (!(await isPasswordValid(user, password))) {
+        res.status(400).send("Invalid credentials");
+        return;
+    }
+
+    const token = generateJWT(user, "30m");
+    res.status(200).json({ token, user });
+});
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers["authorization"]?.split(" ")[1];
+    if (!token) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    const userData = verifyJWT(token);
+    if (!userData) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    req._id = userData._id;
+
+    next();
+};
+
+app.get("/testNeedsAuth", authMiddleware, async (req, res) => {
+  res.send(`You are authenticated as ${req._id}`);
 });
 
 app.listen(port, async () => {
-    console.log(await findAllUsers());
-
-    console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+    console.log(`Example app listening at http://localhost:${port}`);
 });
--- packages/backend/src/models/user.ts
+++ packages/backend/src/models/user.ts
@@ -1,17 +1,30 @@
-import mongoose from "mongoose";
+import mongoose, { type ObjectId } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
+    password: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     createdAt: { type: Date, default: Date.now },
 });
 
+// Before storing 'password', hash it.
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
 export type User = {
     email: string;
-    passwordHash: string;
+    password: string;
     username: string;
     createdAt: Date;
 };
+
+export type UserWithId = User & { _id: ObjectId };
 
 export const User = mongoose.model("User", userSchema, "Users");

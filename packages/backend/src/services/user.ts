be35bcb29325d94a8b4569f8ea91a30962dca32a
David Cruz <david@codebycruz.com>
2025-05-24 22:04:36 -0700

    Progress
    Provides schemas for Posts and Stores, alongside adding posts to the User schema.
    
    Now all schemas are resolved by default, which is more intuitive, although bad for performance. For our case of a demonstration, testing app, where we should have at most hundreds of posts and users, it shouldn't be an issue.

--- packages/backend/src/services/user.ts
+++ packages/backend/src/services/user.ts
@@ -1,54 +1,18 @@
 import { User, UserWithId } from "../models/user";
-import * as bcrypt from "bcrypt";
-import * as jwt from "jsonwebtoken";
 
 export async function findAllUsers(): Promise<UserWithId[]> {
-    return User.find();
+    // @ts-ignore: .populate doesn't properly manipulate the type.
+    return User.find().populate("posts.author").populate("posts.comments.author") as UserWithId[];
 }
 
 export async function findUserByName(username: string): Promise<UserWithId | null> {
-    return User.findOne({ username });
+    // @ts-ignore: .populate doesn't properly manipulate the type.
+    return User.findOne({ username }).populate("posts.author").populate("posts.comments.author") as UserWithId | null;
 }
 
 export async function createUser(username: string, password: string): Promise<UserWithId> {
     const user = new User({ username, password });
 
-    // @ts-ignore: Not sure why their typings don't include _id.
-    // But from my tests, it is included.
+    // @ts-ignore: .save() doesn't add _id to the type.
     return await user.save();
-}
-
-// Auth utilities
-
-export async function isPasswordValid(user: User, password: string): Promise<boolean> {
-    return await bcrypt.compare(password, user.password);
-}
-
-const jwtSecret = process.env.JWT_SECRET;
-if (!jwtSecret) {
-    throw new Error("JWT_SECRET is not set");
-}
-
-const jwtExpiryTime = "30m";
-
-type JWTData = { _id: UserWithId["_id"]; };
-
-export function generateJWT(user: UserWithId): string {
-    return jwt.sign({ _id: user._id }, jwtSecret as string, { expiresIn: jwtExpiryTime })
-}
-
-export function verifyJWT(token: string): JWTData | null {
-    try {
-        return jwt.verify(token, jwtSecret as string) as JWTData;
-    } catch (err) {
-        return null;
-    }
-}
-
-declare global {
-    namespace Express {
-        interface Request {
-            _id?: JWTData["_id"];
-        }
-    }
 }

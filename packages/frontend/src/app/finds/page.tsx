136b3ad10239191978b54ef2bc4f64a861985427
David Cruz <david@codebycruz.com>
2025-06-02 19:25:33 -0700

    Mobile progress

--- packages/frontend/src/app/finds/page.tsx
+++ packages/frontend/src/app/finds/page.tsx
@@ -21,7 +21,7 @@ type PostInfo = {
 
 function Comment(props: { comment: CommentInfo }) {
   return (
-    <div className="bg-gray-700 p-4 rounded-lg">
+    <div className="bg-gray-700 p-2 md:p-4 rounded-lg">
       <h1 className="font-semibold italic">{props.comment.authorUsername}</h1>
       <p>{props.comment.content}</p>
     </div>
@@ -31,12 +31,12 @@ function Comment(props: { comment: CommentInfo }) {
 function CommentSection(props: { comments: CommentInfo[] }) {
 
   return (
-    <div className="h-full pt-20 gap-2 flex flex-col w-1/4">
-      <h1 className="text-xl text-black dark:text-white font-bold">
+    <div className="md:w-1/4 md:h-full md:pt-20 md:gap-2 flex flex-col">
+      <h1 className="md:text-xl text-black dark:text-white font-bold">
         Comments
       </h1>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 md:gap-4">
         {props.comments.map((comment, index) => (
           <Comment comment={comment} key={index}/>
         ))}
@@ -51,7 +51,7 @@ function CommentSection(props: { comments: CommentInfo[] }) {
 
 function ImageSection(props: { authorUsername: string, postImage?: string }) {
   return (
-    <div className="w-2/4 bg-gray-700 p-4 rounded-lg">
+    <div className="w-full md:w-2/4 md:p-4 rounded-lg">
       <div className="flex flex-col h-full gap-2">
         <div className="flex flex-row items-center gap-2">
           <div className="rounded-full bg-gray-500 size-8" />
@@ -81,9 +81,9 @@ function ImageSection(props: { authorUsername: string, postImage?: string }) {
 
 function DescriptionSection(props: { description: string }) {
   return (
-    <div className="w-1/4 flex flex-col gap-2 h-full">
-      <h1 className="font-bold text-lg">Description</h1>
-      <p className="bg-gray-700 p-2 rounded-lg min-h-1/2">
+    <div className="w-full md:w-1/4 flex flex-col md:gap-2 md:h-full">
+      <h1 className="font-bold md:text-lg">Description</h1>
+      <p className="bg-gray-700 p-1 md:p-2 rounded-lg h-32 md:min-h-1/2">
         {props.description}
       </p>
     </div>
@@ -92,11 +92,17 @@ function DescriptionSection(props: { description: string }) {
 
 function Post(props: { post: PostInfo }) {
   return (
-    <div className="p-8 h-screen flex flex-col items-center">
-      <div className="flex flex-row gap-8 h-full w-full rounded-lg p-2 py-4 md:max-w-5xl lg:max-w-7xl xl:max-w-[1920px]">
+    <div className="px-1 md:p-8 md:h-[640px] flex flex-col items-center">
+      <div className="hidden md:flex flex-row gap-8 h-full w-full rounded-lg p-2 py-4 md:max-w-5xl md:max-w-7xl xl:max-w-[1920px]">
         <CommentSection comments={props.post.comments}/>
         <ImageSection authorUsername={props.post.authorUsername} postImage={props.post.imageUrl}/>
         <DescriptionSection description={props.post.description}/>
+      </div>
+
+      <div className="flex md:hidden flex-col w-full">
+        <ImageSection authorUsername={props.post.authorUsername} postImage={props.post.imageUrl} />
+        <DescriptionSection description={props.post.description} />
+        <CommentSection comments={props.post.comments} />
       </div>
     </div>
   );
@@ -117,14 +123,14 @@ export default function Finds() {
   ];
 
   return (
-    <div className="flex flex-col justify-center bg-gray-600">
+    <div className="flex flex-col justify-center">
       {posts.map((post, index) => {
         const isLast = index === posts.length - 1;
 
         return (
           <div key={index}>
             <Post post={post} />
-            {!isLast && <hr className="border-t border-gray-300" />}
+            {!isLast && <hr className="border-t border-gray-300 my-8 md:my-0" />}
           </div>
         );
       })}
